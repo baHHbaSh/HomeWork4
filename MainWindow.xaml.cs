5af2d8087d6d6b9ce022d356f04f25a1254a6901
The_EnG1nE <baHHbaSh@gmail.com>
2025-10-22 10:48:34 +0500

    Fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,46 +30,53 @@ namespace HomeWork4
             {
                 case 0:
                     MessageBox.Show("Я съел торт :)"); return 0;
+                case 1:
+                    MessageBox.Show("А тебе зачем?"); return 0;
                 default:
                     return n % 2 == 0 ? n / 2 : n;
             }
         }
-        static private bool Chess(byte x, byte y)
+        static private bool Chess(byte x, byte y, byte z, byte w)
         {
-            if (x > 8 || y > 8 || x == 0 || y == 0)
+            if (x > 8 || y > 8 || x == 0 || y == 0 || z > 8 || w > 8 || z == 0 || w == 0)
                 MessageBox.Show("Недопустимая позиция");
-            return (x + y) % 2 == 0;
+            return (x + y) % 2 == (z + w) % 2;
         }
         static private int Pool(int k, int l, int m, int n)
         {
-            return Math.Min(Math.Min(k, l), Math.Min(m, n));
+            if (k > l)
+                (l, k) = (k, l);
+            if (l-m < 0 || k - n < 0)
+                (m, n) = (n, m);
+            return Math.Min(Math.Min(m, n), Math.Min(l - m, k - n));
+
         }
         static private bool Brick(int a, int b, int c, int d, int e)
         {
-            if (
+            return (
                     a < d && b < e || a < e && b < d ||
                     b < d && c < e || b < e && c < d ||
                     a < d && c < e || a < e && c < d
-                )
-                return true;
-            return false;
+                );
         }
         static private string Bochki(int n)
         {
+            int lastTwoDigits = n % 100;
+            if (lastTwoDigits >= 11 && lastTwoDigits <= 14)
+            {
+                return $"{n} bochek";
+            }
             switch (n % 10)
             {
-                case 1: return $"{n} bochka";
+                case 1:
+                    return $"{n} bochka";
                 case 2:
                 case 3:
-                case 4: return $"{n} bochki";
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                case 0: return $"{n} bochek";
+                case 4:
+                    return $"{n} bochki";
+                default:
+                    return $"{n} bochek";
             }
-            return ""; // От ошибок
         }
 
         private void Z1R_Click(object sender, RoutedEventArgs e)
@@ -79,7 +86,7 @@ namespace HomeWork4
 
         private void Z2R_Click(object sender, RoutedEventArgs e)
         {
-            var n = Chess(byte.Parse(Z2X.Text), byte.Parse(Z2Y.Text)) ? "чёрная" : "белая";
+            var n = Chess(byte.Parse(Z2X.Text), byte.Parse(Z2Y.Text), byte.Parse(Z2Z.Text), byte.Parse(Z2W.Text)) ? "чёрная" : "белая";
             Z2R.Content = $"Клетка { n }";
         }
 
